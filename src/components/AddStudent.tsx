fea348db227a60a64a1b0eba70aeeeea1cc23bf2
sharifabee <sharifabee.alur@gmail.com>
2025-02-19 09:50:23 +0530

    add stu form

--- src/components/AddStudent.tsx
+++ src/components/AddStudent.tsx
@@ -5,6 +5,47 @@ export default function AddStudent() {
   return (
     <div>
       <NavBar />
-      AddStudent</div>
+      
+      <form>
+  
+  <div className="form-group">
+    <label >RollNo</label>
+    <input type="text" className="form-control" 
+    id="exampleInputEmail1" aria-describedby="emailHelp" 
+    placeholder="Enter Roll no" />
+   </div>
+
+  <div className="form-group">
+    <label >Name</label>
+    <input type="text" className="form-control" 
+    id="exampleInputEmail1" aria-describedby="emailHelp" 
+    placeholder="Enter Name" />
+   </div>
+  <div className="form-group">
+    <label >Marks</label>
+    <input type="text" className="form-control" 
+    id="exampleInputEmail1" aria-describedby="emailHelp" 
+    placeholder="Enter Marks " />
+   </div>
+  <div className="form-group">
+    <label >Fee</label>
+    <input type="text" className="form-control" 
+    id="exampleInputEmail1" aria-describedby="emailHelp" 
+    placeholder="Enter Fee" />
+   </div>
+  <div className="form-group">
+    <label >Course</label>
+    <input type="text" className="form-control" 
+    id="exampleInputEmail1" aria-describedby="emailHelp" 
+    placeholder="Enter Course" />
+   </div>
+  
+  
+  
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+      
+      
+      </div>
   )
 }
